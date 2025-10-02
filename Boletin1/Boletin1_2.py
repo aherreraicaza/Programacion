fc76779af6243012513b22afb17e81fd8d3e1d1c
Antuan <aherreraicaza@danielcastelao.org>
2025-10-02 14:00:20 +0200

    Boletin1_2.py acabado

---
+++ Boletin1/Boletin1_2.py
@@ -0,0 +1,4 @@
+a) Salto- mortal , salto_mortal, salto + mortal, 2salto, “salto”
+Salto- mortal, salto + mortal, 2salto, “salto”
+b) cantidade total, cant_total, cant5, cantidadeTotal
+cantidade total
