c4f99b63230e3cf99f9e0d75625b1bc164b94297
Antuan <aherreraicaza@danielcastelao.org>
2025-10-02 14:40:59 +0200

    Boletin1_5.py acabado

---
+++ Boletin1/Boletin1_5.py
@@ -0,0 +1,10 @@
+a) i = 1, j = 0, k = 1 				i + k <= j – k * 3 and k >= 2           false
+b) i = 3, j = 2, k = -1				i == 3 or j <= 2 and k > 0              true
+c) tipo = 10, rede = 7,5 			tipo < rede + 1,5                       false
+d) ano = 1993				ano % 400 == 0                                  false
+e) 3 == 2 or 5 > 1 + 1                                                      true
+f ) 5 – 2 > 4 and not(0,5 == 1 / 5)                                         false
+g) a = 2, b = 5, c = 6, d = 10		a >= b or a >= c and a <d               false
+h) a = 2, b = 5, c = 6, d = 10	    a + b < c and a + c < d or 2 * a < a + b true
+i)  a = 2, b = 5, c = 6, d = 10		!(a * b < d) and !(a * b < c) or b + c <= d  true
+
