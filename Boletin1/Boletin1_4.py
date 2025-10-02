63f476d813ea743051573922e5190353f20a8d05
Antuan <aherreraicaza@danielcastelao.org>
2025-10-02 14:16:04 +0200

    Boletin1_4.py acabado

---
+++ Boletin1/Boletin1_4.py
@@ -0,0 +1,7 @@
+a) true and true == false                 false
+b) not false == true                      true
+c) (true and true) or false == true       true
+d) (false or false) and false != true     false
+e) (not(true and false)) == false         false
+f) “12” + “12” == “24”                    false
+g) “34” + “43” == “3443”                  true
