3274a3a5bf451bcf858ff479f1282e7385dc7e47
Antuan <aherreraicaza@danielcastelao.org>
2025-10-03 10:38:07 +0200

    Boletin1_5.py acabado

---
+++ Boletin2/Boletin2_4.py
@@ -0,0 +1,14 @@
+numero1 = float(input("Introduce el primer número: "))
+numero2 = float(input("Introduce el segundo número: "))
+
+suma = numero1 + numero2
+print(suma)
+
+resta = numero1 - numero2
+print(resta)
+
+producto = numero1 * numero2
+print(producto)
+
+cociente = numero1 / numero2
+print(cociente)
