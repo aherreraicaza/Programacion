1166894310646945549a26fd35202d7c82c8a4e5
Antuan <aherreraicaza@danielcastelao.org>
2025-10-09 12:48:20 +0200

    Boletin3_2.py acabado

---
+++ Boletin3/Boletin3_2.py
@@ -0,0 +1,8 @@
+numero_1 = int(input("Dime un número: "))
+numero_2 = int(input("Dime outro número: "))
+
+if numero_1 >= numero_2:
+    print("resta:", numero_1 - numero_2)
+
+print("suma:", numero_1 + numero_2)
+
