606220e9fa97a2a2846e3670cd90fc56e17104b1
Antuan <aherreraicaza@danielcastelao.org>
2025-10-09 12:54:05 +0200

    Boletin3_4.py acabado

---
+++ Boletin3/Boletin3_4.py
@@ -0,0 +1,7 @@
+numero_1 = float(input("Introduce o primeiro número: "))
+numero_2 = float(input("Introduce o segundo número: "))
+
+if numero_1 >= numero_2:
+    print("Resta:", numero_1 - numero_2)
+
+print("Suma:", numero_1 + numero_2)
