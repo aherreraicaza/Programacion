58059bf63b9fc79b38c911e935146c0b52d31b92
Antuan <aherreraicaza@danielcastelao.org>
2025-10-16 13:54:31 +0200

    feat(Boletín6_8.py): acabado

---
+++ Boletin6/Boletín6_8.py
@@ -0,0 +1,6 @@
+lista_precios = [50, 75, 46, 22, 80, 65, 8]
+precio_menor = min(lista_precios)
+precio_mayor = max(lista_precios)
+
+print("El precio menor es: ",precio_menor)
+print("El precio mayor es: ",precio_mayor)
