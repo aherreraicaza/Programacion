6a15a2003fea27c62edb8d5c85043d1c542948e3
Antuan <aherreraicaza@danielcastelao.org>
2025-10-17 10:46:37 +0200

    feat(Boletin7_8.py): acabado

---
+++ Boletin7/Boletin7_8.py
@@ -0,0 +1,14 @@
+cadena = "Python"
+cadena2 = "JavaScript"
+
+comparacion = cadena == cadena2
+print("¿Las cadenas son iguales?:", comparacion)
+
+if cadena == cadena2:
+    print("Las cadenas son iguales")
+
+else:
+    print("Las cadenas no son iguales")
+
+
+
