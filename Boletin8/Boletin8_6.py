ac411eb6de64ae89e0227c03b433c1dee3d28fda
Antuan <aherreraicaza@danielcastelao.org>
2025-12-05 11:06:41 +0100

    feat(Boletin8_6.py): acabado

---
+++ Boletin8/Boletin8_6.py
@@ -0,0 +1,42 @@
+def es_primo(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    resultado = 1
+    for i in range(2, num + 1):
+        resultado *= i
+    return resultado
+
+def procesar_numeros(lista_numeros):
+
+    primos = []
+    for num in lista_numeros:
+        if es_primo(num):
+            primos.append(num)
+
+    sumatorio = sum(lista_numeros)
+
+    if lista_numeros:
+        promedio = sumatorio / len(lista_numeros)
+    else:
+        promedio = 0
+
+    factoriales = []
+    for num in lista_numeros:
+        factoriales.append(factorial(num))
+
+    return primos, sumatorio, promedio, factoriales
+
+numeros = [3, 4, 5, 6, 7, 8, 9, 10]
+primos, sumatorio, promedio, factoriales = procesar_numeros(numeros)
+print(f'Números primos: {primos}')
+print(f'Sumatorio: {sumatorio}')
+print(f'Promedio: {promedio}')
+print(f'Factoriales: {factoriales}')
